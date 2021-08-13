f352df5948a671dcdf40a883ebb4b64ddd50c3f8
issam hadjal <issam.hadjal@gmail.com>
2021-08-13 16:59:59 +0200

    watch-list before front

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,48 +1,39 @@
 class ListsController < ApplicationController
-    before_action :set_list, only: %i[show edit update destroy]
+  before_action :set_list, only: [:show, :destroy]
 
-    def index
-      @lists = List.all
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
     end
-  
-    def show; end
-  
-    def new
-      @list = List.new
-    end
-  
-    def edit; end
-  
-    def create
-      @list = List.new(list_params)
-  
-      if @list.save
-        redirect_to @list, notice: 'list was successfully created.'
-      else
-        render :new
-      end
-    end
-  
-    def update
-      if @list.update(list_params)
-        redirect_to @list, notice: 'list was successfully updated.'
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @list.destroy
-      redirect_to lists_url, notice: 'list was successfully destroyed.'
-    end
-  
-    private
-  
-    def set_list
-      @list = List.find(params[:id])
-    end
-  
-    def list_params
-      params.require(:list).permit(:name)
-    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
